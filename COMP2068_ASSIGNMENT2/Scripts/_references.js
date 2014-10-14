be813e6ae7ba8a7184a9a9a78b2207417b62c93c
parkjonghwa75 <jaypark.1120@gmail.com>
2014-10-14 14:34:46 -0400

    added image files

--- COMP2068_ASSIGNMENT2/Scripts/_references.js
+++ COMP2068_ASSIGNMENT2/Scripts/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path ="jquery-2.1.1.js" />
 /// <reference path="easeljs-0.7.1.min.js" />
+/// <reference path="main.js" />
 
--- COMP2068_ASSIGNMENT2/Scripts/main.js
+++ COMP2068_ASSIGNMENT2/Scripts/main.js
@@ -1,14 +1,19 @@
-﻿var stage: createjs.Stage;
+﻿var stage;
 
 function init() {
     stage = new createjs.Stage(document.getElementById("mainCanvas"));
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", handleTick);  
-
-    alert("woohoo");
+    
+    drawSlotMachine();
 }
 
 function handleTick() {
     stage.update();
 }
 
+function drawSlotMachine() {
+    var slotMachine = new createjs.Bitmap("img/mainBoardoard.jpg");
+
+    stage.addChild(slotMachine);
+}
